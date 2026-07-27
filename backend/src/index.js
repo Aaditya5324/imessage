10955dc1b5961e4c09707d3c2f47e46cc9fef0f3
Aditya Yadav <ay0274001@gmail.com>
2026-07-27 19:22:25 +0530

    Add Dockerfile, .dockerignore, and integrate Clerk authentication in frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,11 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config.js";
+import fs from "fs";
+import path from "path";
 import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from './lib/db.js';
+
 import User from './models/user.model.js';
 const app = express();
 const port = 3000;
@@ -21,6 +24,13 @@ app.get('/h', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${port}`);

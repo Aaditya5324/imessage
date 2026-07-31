21dfd8611814b3386e98739be10564672fe8a870
Aditya Yadav <ay0274001@gmail.com>
2026-07-31 19:38:30 +0530

    fix: add missing import for Toaster component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { useAuth } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
